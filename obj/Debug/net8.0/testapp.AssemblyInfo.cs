58000cccf9ff34d74b146a145103d1de6e8ad53a
mk74936 <117074826+mk74936@users.noreply.github.com>
2025-04-08 18:21:29 -0400

    removed .vs from git vision

--- obj/Debug/net8.0/testapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/testapp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ab5a2a0446b3576a5723ac5ba7b7988509ffae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b1c2163f29240bdaf09f83422fc3023d212b763")]
 [assembly: System.Reflection.AssemblyProductAttribute("testapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
